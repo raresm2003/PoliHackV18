f5e5899b4716849017083dbe5e8fe73f5af8e1fa
raresm2003 <miclearares@gmail.com>
2025-12-05 18:46:57 +0200

    initialised backend

---
+++ PH-Backend/src/main/java/org/example/phbackend/PhBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.phbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhBackendApplication.class, args);
+	}
+
+}
